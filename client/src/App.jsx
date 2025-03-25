cb8d27a0dd4b38b627c43cb526084fb5deb59585
Gantz <lli12@syr.edu>
2025-03-24 20:48:33 -0400

    url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api'
 });
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.NODE_ENV === 'production' ? '/api' :'https://store-collector-api.vercel.app/api'
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
